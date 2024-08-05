362f6d2e4820a1b1b97da1d9243fc1f32c24fc43
Manuel Pinheiro <manuel.pinheiro@libero.it>
2024-08-05 15:07:25 +0200

    NuGet

---
+++ BW-Clinica-Veterinaria/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BW_Clinica_Veterinaria.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
