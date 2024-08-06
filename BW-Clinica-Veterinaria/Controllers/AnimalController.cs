522c2041eea950cdeded296f5be65d239bd57b4e
AlessandroBianco00 <alessandrobianco2000@gmail.com>
2024-08-06 10:49:42 +0200

    Creata View per aggiunta animale + actions nel controller

--- BW-Clinica-Veterinaria/Controllers/AnimalController.cs
+++ BW-Clinica-Veterinaria/Controllers/AnimalController.cs
@@ -1,12 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BW_Clinica_Veterinaria.Context;
+using BW_Clinica_Veterinaria.Interface;
+using BW_Clinica_Veterinaria.Models.Entity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BW_Clinica_Veterinaria.Controllers
 {
     public class AnimalController : Controller
     {
+        private readonly DataContext _ctx;
+        private readonly IAnimalService _animalService;
+
+        public AnimalController(DataContext dbContext, IAnimalService animalService)
+        {
+            _ctx = dbContext;
+            _animalService = animalService;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult AggiungiAnimale()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AggiungiAnimale(Animale animale)
+        {
+            await _animalService.AggiungiAnimale(animale);
+            return RedirectToAction("Index", "Home");
         }
     }
 }
