07545f1f087417bc5985be6afbc5778f63dc561c
Morgan Grande <morgan.grande@gmail.com>
2024-08-06 13:14:38 +0200

    program

--- BW-Clinica-Veterinaria/Program.cs
+++ BW-Clinica-Veterinaria/Program.cs
@@ -13,7 +13,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddScoped<IProdottoService, ProdottoService>();
+
 
 builder.Services
     .AddScoped<IAnimalService, AnimalService>()
