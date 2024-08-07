ff2b8bfd652cdc3d1c9c97e25743ee27a6ef666b
AlessandroBianco00 <alessandrobianco2000@gmail.com>
2024-08-07 11:18:58 +0200

    Create Policies

---
+++ BW-Clinica-Veterinaria/Policies.cs
@@ -0,0 +1,9 @@
+﻿namespace BW_Clinica_Veterinaria
+{
+    public class Policies
+    {
+        public const string LoggedIn = "LoggedIn_Policy";
+        public const string IsVet = "IsVet_Policy";
+        public const string IsPharmacist = "IsPharmacist_Policy";
+    }
+}
